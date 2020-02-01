ad04cb147bf76ffa12c4757ccfe305f743cdca52
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-01 16:35:16 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -6,11 +6,12 @@ def say_hello_world_ten_times
   puts phrase
   puts phrase
   puts phrase
-puts phrase
   puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
 
+say_hello_world_ten_times
